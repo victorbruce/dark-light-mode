535810dcc1774cd2d0769ae56aa44a54a8a09e14
victorbruce <victorbruce82@gmail.com>
2020-08-11 18:39:53 +0000

    feat: app assets

---
+++ src/components/Icons/LightMode.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+const LightMode = () => {
+  return (
+    <svg
+      width="34"
+      height="34"
+      viewBox="0 0 24 24"
+      fill="none"
+  
+    >
+      <path
+        d="M12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17Z"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 1V3"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 21V23"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.22 4.22L5.64 5.64"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.36 18.36L19.78 19.78"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1 12H3"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21 12H23"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.22 19.78L5.64 18.36"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.36 5.64L19.78 4.22"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default LightMode;
